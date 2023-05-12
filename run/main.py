989e76cb6a1dbc12a8af3252f9af3e9e97e10825
Mikhail Kotyushev <mkotyushev@gmail.com>
2023-05-12 22:45:32 +0600

    Add utils; Add CV main

--- run/main.py
+++ run/main.py
@@ -1,8 +1,20 @@
-from lightning.pytorch.cli import LightningCLI
+import logging
+logging.basicConfig(
+    level=logging.INFO,
+    format = '[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s'
+)
+
+from src.utils.utils import MyLightningCLI, TrainerWandb
 
 
 def cli_main():
-    cli = LightningCLI()
+    cli = MyLightningCLI(
+        trainer_class=TrainerWandb, 
+        save_config_kwargs={
+            'config_filename': 'config_pl.yaml',
+            'overwrite': True,
+        }
+    )
 
 
 if __name__ == "__main__":
